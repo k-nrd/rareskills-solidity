8727976d19e8e1fed8d7cf3cc9a75eab7a2f320a
k-nrd <g.konrad@outlook.com>
2023-12-25 19:26:25 +0100

    fixing stuff

--- week2/contracts/src/trio/NFTShare.sol
+++ week2/contracts/src/trio/NFTShare.sol
@@ -1,8 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.23;
 
-import {ERC20PrivateMint} from "../lib/ERC20PrivateMint.sol";
+import {ERC20Private} from "../lib/ERC20Private.sol";
 
-contract NFTShare is ERC20PrivateMint {
-    constructor(address _vault) ERC20PrivateMint("CryptoHipposShare", "CHPS", _vault) {}
+contract NFTShare is ERC20Private {
+    constructor(address _vault) ERC20Private("CryptoHipposShare", "CHPS", _vault) {}
 }
