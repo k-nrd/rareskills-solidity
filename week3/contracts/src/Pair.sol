586de6148c7ed4b029499cbd161615fba2df047a
k-nrd <g.konrad@outlook.com>
2024-01-18 18:45:31 -0300

    started working on pair contract

---
+++ week3/contracts/src/Pair.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.23;
+
+contract Pair {
+    address public immutable token1;
+    address public immutable token2;
+
+    function deposit() {}
+
+    function withdraw() {}
+
+    function swap() {}
+}
