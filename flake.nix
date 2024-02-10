5b42a3dceaabe09d3b94a3115ce739b5f9142983
k-nrd <g.konrad@outlook.com>
2024-02-10 18:40:54 -0300

    flake w solc-select

--- flake.nix
+++ flake.nix
@@ -4,27 +4,20 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     utils.url = "github:numtide/flake-utils";
     foundry.url = "github:shazow/foundry.nix/monthly"; # Use monthly branch for permanent releases
-    solc = {
-      url = "github:hellwolf/solc.nix";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
   };
-  outputs = { self, nixpkgs, utils, foundry, solc }: 
+  outputs = { self, nixpkgs, utils, foundry }: 
     utils.lib.eachDefaultSystem (system: 
       let 
         pkgs = import nixpkgs {
           inherit system;
           overlays = [ 
             foundry.overlay 
-            solc.overlay
           ];
         };
       in {
         devShell = with pkgs; mkShell {
           buildInputs = [
-            solc_0_8_19
-            solc_0_8_23
-            (solc.mkDefault pkgs solc_0_8_23) # Specify a default solc version
+            solc-select
             foundry-bin 
             slither-analyzer
             echidna
